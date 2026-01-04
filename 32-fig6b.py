0e00451698da6ca73dd77e5cb4ea1a25274a8317
Luessiaw <Luessiaw@pku.edu.cn>
2026-01-04 15:34:01 +0800

    绘制图6b

--- 32-fig6b.py
+++ 32-fig6b.py
@@ -61,45 +61,11 @@ def getMoment(p:Paras,sigma:float):
     return Bm,rs,width
 
 view_init = [30,-30]
-rp_phis = np.array([-50,0])/180*np.pi
+radius = 0.09
+ds = np.array([3.5,4.5,5.5])/1e2
 rp_theta = 45/180*np.pi
-p = np.array([0,10e-9,0])
-rps = []
-ps = []
-for i,rp_phi in enumerate(rp_phis):
-    rp = np.array([np.sin(rp_theta)*np.cos(rp_phi),
-                np.sin(rp_theta)*np.sin(rp_phi),
-                np.cos(rp_theta)])*9e-2
-    rps.append(rp)
-    p = np.cross(unit_z,rp)
-    p = 10e-9*p/np.linalg.norm(p)
-    ps.append(p)
+rp_phis = -np.array([2*np.arcsin(d/radius/2/np.sin(rp_theta)) for d in ds])
 
-# fig6a-1 绘制源
-fig1 = vs.plt.figure()
-fig1.set_size_inches(10,8)
-vz = Visualizer()
-
-fig1,ax1 = vz.create3DAxis(3,fig=fig1,lims=[0.15,0.15,0.15])
-ax1.view_init(elev=view_init[0], azim=view_init[1])
-ax1.set_axis_off()
-
-vz.showHead(paras.radiusOfHead,3,ax1,alpha=0.1)
-
-for i in range(len(rps)):
-    vz.showSource(rps[i],ps[i],ax1,3,
-                arrowBottomRadius=0.002,arrowTipRadius=0.005,
-                arrowBottomLength=0.02,arrowTipLength=0.01,
-                color="red")
-
-# vz.showAxisArrow(ax1,0.11,0.11,0.11,
-#                 bottomRadius=0.0005,tipRadius=0.002,
-#                 xTipLength=0.01,yTipLength=0.01,zTipLength=0.01)
-
-fig1.savefig("figs/fig6a/6a-1.png",
-             dpi=300,bbox_inches='tight', pad_inches=0)
-vs.plt.close(fig1)
-print("Done")
 
 # fig6a-2 对两个源进行源成像
 paras.gridSpacing = 0.3e-2
@@ -121,69 +87,63 @@ for (k,par) in enumerate([paras3s,paras3v]):
     print(f"num of source points: {sol.sourcePoints.shape[1]}")
     points = sol.sourcePoints
     distance = np.sqrt(points[0,:]**2+points[1,:]**2+points[2,:]**2)
-    radius = sol.paras.sourceOnSpheres[-1] # 取最外层
-    # mask = (distance<radius*1.1)&(distance>radius*0.9)&(points[2,:]>=0)
-    mask = (distance<0.15)&(distance>0.06)
+    mask = (distance<0.092)&(distance>0.089)&(points[2,:]>=0)
     xs = points[0,mask]
     ys = points[1,mask]
     zs = points[2,mask]
 
-    Qs = []
+    for rp_phi in rp_phis:
+        d = 9*np.sin(rp_theta)*np.sin(-rp_phi/2)*2
+        print(f"Distance: {d:.1f} cm.")
+        
+        rps = []
+        ps = []
+        for rp_phi2 in [0,rp_phi]:
+            rp = np.array([np.sin(rp_theta)*np.cos(rp_phi2),
+                        np.sin(rp_theta)*np.sin(rp_phi2),
+                        np.cos(rp_theta)])*9e-2
+            rps.append(rp)
+            p = np.cross(unit_z,rp)
+            p = 10e-9*p/np.linalg.norm(p)
+            ps.append(p)
 
-    # 仅源1或源2打开
-    for j in range(2):
-        rp = rps[j]
-        p = ps[j]
-        Bm = sol.getBm(rp,p)
+        Bm = 0
+        for j in range(2):
+            Bm += sol.getBm(rps[j],ps[j],not j)
         Q = sol.applyInverse(Bm)
-        Qs.append(Q)
-
-    # 源1和源2同时打开
-    Q = Qs[0] + Qs[1]
-    Qs.append(Q)
-    for j in range(3):
-        QA = sol.getQAmplitute(Qs[j])
+        QA = sol.getQAmplitute(Q)
         QAm = QA[mask]
         QAm /= QAm.max()
+        # X,Y,V = interpXYs(xs,ys,QAm,nx=200,ny=200,xlim=[xs.min(),xs.max()],ylim=[ys.min(),ys.max()],method="cubic")
+        radiusOfTheta = radius*np.sin(rp_theta)
         
         fig2 = vs.plt.figure()
         fig2.set_size_inches(10,8)
         vz = Visualizer()
-
         fig2,ax2 = vz.create3DAxis(par.dim,fig=fig2,lims=[0.15,0.15,0.15])
-        # ax2.set_axis_off()
+        ax2.set_proj_type('ortho')
         ax2.view_init(elev=view_init[0], azim=view_init[1])
 
-        # 重新绘制 box
-        ax2.set_axis_off()
         xlim = np.array([-0.1,0.1])
         ylim = np.array([-0.1,0.1])
-        # zlim = np.array([-0.01,1.01])
-        zlim = np.array([-0.1,0.1])
-        # kwargs = {"color":"k","lw":1}
-        # # 绘制底面边线
-        # ax2.plot(xlim[[0,1]],ylim[[0,0]],zlim[[0,0]],**kwargs)
-        # # ax2.plot(xlim[[0,1]],ylim[[1,1]],zlim[[0,0]],**kwargs)
-        # # ax2.plot(xlim[[0,0]],ylim[[0,1]],zlim[[0,0]],**kwargs)
-        # ax2.plot(xlim[[1,1]],ylim[[0,1]],zlim[[0,0]],**kwargs)
-        # # 绘制顶面边线
-        # ax2.plot(xlim[[0,1]],ylim[[0,0]],zlim[[1,1]],**kwargs)
-        # ax2.plot(xlim[[0,1]],ylim[[1,1]],zlim[[1,1]],**kwargs)
-        # ax2.plot(xlim[[0,0]],ylim[[0,1]],zlim[[1,1]],**kwargs)
-        # ax2.plot(xlim[[1,1]],ylim[[0,1]],zlim[[1,1]],**kwargs)
-        # # 绘制竖线
-        # ax2.plot(xlim[[0,0]],ylim[[0,0]],zlim[[0,1]],**kwargs)
-        # # ax2.plot(xlim[[0,0]],ylim[[1,1]],zlim[[0,1]],**kwargs)
-        # # ax2.plot(xlim[[1,1]],ylim[[0,0]],zlim[[0,1]],**kwargs)
-        # ax2.plot(xlim[[1,1]],ylim[[1,1]],zlim[[0,1]],**kwargs)
+        zlim = np.array([-0.01,1.01])
 
-        ax2.scatter(xs,ys,zs,c=QAm,cmap="Reds",s=45)
+        # norm = vs.plt.Normalize(vmin=0, vmax=1)  # 归一化到 [0,1]
+        # cmap = vs.cm.Reds  # 选择颜色映射（如 'viridis', 'jet', 'plasma'）
+        # colors = cmap(norm(V))
+        # ax2.plot_surface(X,Y,V,facecolors=colors,edgecolor="none",shade=False)
+        ax2.scatter(xs,ys,QAm,c=QAm,cmap="Reds",s=250)
         ax2.set(xlim=xlim,ylim=ylim,zlim=zlim)
-        ax2.set(xticks=[],yticks=[],zticks=[])
+
+        # ax2.grid(True, linewidth=5)
+        ax2.tick_params(axis='x', labelbottom=False)
+        ax2.tick_params(axis='y', labelleft=False)
+        ax2.tick_params(axis='z', labelleft=False)
+
         ax2.set_aspect("equalxy")
 
         fig2.canvas.draw()
-        fig2.savefig(f"figs/fig6a/6a-2-{par.getLabel()}-{j}.png",
+        fig2.savefig(f"figs/fig6b/6b-{par.getLabel()}-{d:.1f}cm.png",
                     dpi=300,bbox_inches='tight',pad_inches=0)
         vs.plt.close(fig2)
 
